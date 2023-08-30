e3c178dcafa5221c20723ebe91a7527a7a4f5f36
jeferson2020 <jeferson@lopessoftware.com.br>
2023-08-30 00:39:55 -0300

    formulario de produtos

--- components/Form/Products.vue
+++ components/Form/Products.vue
@@ -7,22 +7,30 @@
                         <p>Vamos começar identificando seu produto.</p>
                     </h2>
                 </div>
-                <div class="col-lg-8 col-md-12 col-sm-12 col-12">
+                <div class="col-lg-8 col-md-12 col-sm-12 col-12" v-if="showpredict">
                     <PartialsProductsMainFeatures @handleSubmitPredict="handleSubmitPredict"/>
                 </div>
-                <div class="col-lg-8 col-md-12 col-sm-12 col-12" v-show="showcategory">
-                    <PartialsProductsCategory :formList="formList" @handleGetSelected="handleGetSelected"/>
-                </div>               
+                <div class="col-lg-8 col-md-12 col-sm-12 col-12" v-if="showcategory" >
+                    <PartialsProductsCategory :formList="formList" @handleGetSelected="handleGetSelected" />                                     
+                </div>
+                <div class="col-lg-8 col-md-12 col-sm-12 col-12" v-if="showlistcategory">
+                    <h4>Categoria</h4>
+                    <Listbox :options="formList" optionLabel="name"/>                    
+                </div>
+                <div class="col-lg-8 col-md-12 col-sm-12 col-12 d-grid gap-3 d-md-flex justify-content-end" v-if="showlistcategory">
+                    <Button label="Pesquisar outra categoria" outlined @click="{ showpredict = true; showlistcategory = false; showcomponent = 0 }" />
+                </div>
+              
                 <!-- <div class="row justify-content-lg-center"> -->
                     <div class="col-lg-8 col-md-12 col-sm-12 col-12 g-4">
-                        <PartialsProductsAutoComponent :id="1" :attribute_id="0" v-if="showcomponent === 1" :component="'TEXT_INPUT'" :label="'Preencha o título do anúncio'" :options="itemWarratyOption" :hint="'Inclua somente o produto, marca, modelo e características principais. Lembre-se de que, quando receber uma venda, você nao poerá editá-lo'" :tooltip="''" :name="'titleitem'" @handleConfirm="handleConfirm" :componentKey="1"/>
+                        <PartialsProductsAutoComponent :id="1" :attribute_id="0" v-if="showcomponent === 1" :component="'TEXT_INPUT'" :label="'Preencha o título do anúncio'" :options="itemWarratyOption" :hint="'Inclua somente o produto, marca, modelo e características principais. Lembre-se de que, quando receber uma venda, você nao poerá editá-lo'" :tooltip="''" :name="'titleitem'" @handleConfirm="handleConfirm" @handleNaoAplica="handleNaoAplica" :hidden="false" :componentKey="1"/>
                    
-                        <PartialsProductsAutoComponent :id="2" :attribute_id="0" v-if="showcomponent === 2" :component="'COMBO'" :label="'Você oferece garantia?'" :options="itemWarratyOption" :hint="''" :tooltip="'Informe se o produto terá garantia.'" :name="'itemWarraty'" @handleConfirm="handleConfirm" :componentKey="2"/>
+                        <PartialsProductsAutoComponent :id="2" :attribute_id="0" v-if="showcomponent === 2" :component="'COMBO'" :label="'Você oferece garantia?'" :options="itemWarratyOption" :hint="''" :tooltip="'Informe se o produto terá garantia.'" :name="'itemWarraty'" @handleConfirm="handleConfirm" @handleNaoAplica="handleNaoAplica" :componentKey="2" :hidden="false" />
                    
-                        <PartialsProductsAutoComponent :id="3" :attribute_id="0" v-if="showcomponent === 3" :component="'CURRENCY_INPUT'" :label="'Preço'" :hint="'Informe o preço do produto.'" :tooltip="'Preço de comercialização do produto.'" :options="[]" :name="'price'"  @handleConfirm="handleConfirm" :componentKey="3"/>
+                        <PartialsProductsAutoComponent :id="3" :attribute_id="0" v-if="showcomponent === 3" :component="'CURRENCY_INPUT'" :label="'Preço'" :hint="'Informe o preço do produto.'" :tooltip="'Preço de comercialização do produto.'" :options="[]" :name="'price'"  @handleConfirm="handleConfirm" @handleNaoAplica="handleNaoAplica" :componentKey="3" :hidden="false" />
                  
                         <span v-for="(group) in componentData">
-                            <PartialsProductsAutoComponent v-if="showcomponent === group.position" :id="group.position" :attribute_id="group.id" :component="group.component" :label="group.name" :hint="group.hint" :tooltip="group.tooltip" :options="group.options" :name="group.ml_attribute_id"  @handleConfirm="handleConfirm" :componentKey="group.position" />                       
+                            <PartialsProductsAutoComponent v-if="showcomponent === group.position" :id="group.position" :attribute_id="group.id" :component="group.component" :label="group.name" :hint="group.hint" :tooltip="group.tooltip" :options="group.options" :name="group.ml_attribute_id"  @handleConfirm="handleConfirm" @handleNaoAplica="handleNaoAplica" :componentKey="group.position"  :hidden="group.hidden"  />                       
                         </span>
                     </div>
                    
@@ -47,13 +55,15 @@ export default {
             componentData: {},
             showcomponent: 0,
             showcategory: false,
+            showlistcategory: false,
             itemWarratyOption:[
                 { code: '00', name: 'Selecione'},
                 { code: '01', name: 'Com garantia' },
                 { code: '02', name: 'Sem garantia' }
             ],
             formData: [],
-            increment: 0
+            increment: 0,
+            showpredict: true
         };
     },
     methods: {
@@ -68,7 +78,7 @@ export default {
             status = status ?? (responseError.value ? responseError.value.statusCode : null);
             this.handleForm(responseData._rawValue.data);            
         },
-        async handleSubmitPredict( value ){    
+        async handleSubmitPredict( value ){              
             this.componentData = [];   
             this.formList = [];     
             const preditor = new Preditor();
@@ -85,10 +95,15 @@ export default {
                 });
                 this.formList = form;
             }
+            this.showpredict = false;  
         },
         async handleGetSelected( value ){
             if( value?.code ){
+                this.formList = [];
                 this.handleGetAttributeByCategoryId( value.code );
+                this.formList[0] = value;
+                this.showcategory = false;
+                this.showlistcategory = true;
             }            
         },
         async handleForm( value ){
@@ -96,27 +111,29 @@ export default {
             this.increment = 0;
             const comp = [];
 
-            value.forEach(function(value, key){
-                comp[key] = value;
-
-            });
-
             value.forEach(function( value, key ){               
                 let opt = value.values ?  JSON.parse(value.values) : [];
                 let options = [];
                 opt.forEach(function( valueOpt, keyOpt ){
                     options[keyOpt] = {code: valueOpt.id, name: valueOpt.name};
-                });
+                });                
                 comp[key] = value;
                 comp[key].position = (key+4);
                 comp[key].options = options;
+                comp[key].required = JSON.parse(value.tags)?.required ? true : false;
+                comp[key].hidden = JSON.parse(value.tags)?.hidden ? true : false;
+                comp[key].variation_attribute = JSON.parse(value.tags)?.variation_attribute ? true : false;
+                comp[key].catalog_required = JSON.parse(value.tags)?.catalog_required ? true : false; 
+               
             });
             this.componentData = comp;
         },
         async handleConfirm( value ){           
-           this.formData[value.name] = value;
-           this.showcomponent = (value.position+1);
-           console.log(this.showcomponent);
+            this.formData[value.name] = value;
+            this.showcomponent = (value.position + 1);
+        },
+        async handleNaoAplica( key ){
+            this.showcomponent = (key + 1);
         },
         mounted(){
             this.increment = 1;
--- components/partials/Products/AutoComponent.vue
+++ components/partials/Products/AutoComponent.vue
@@ -1,16 +1,26 @@
 <template>
     <Card>
-        <template #title >{{ label }} 
-            <span  v-Tooltip.top="tooltip" v-if="tooltip">
-                <i class="pi pi-question-circle"></i>
-            </span>
+        <template #title>
+            <div class="row">
+                <div class="col-lg-10 col-md-10 col-sm-10 col-10">
+                    {{ label }}<span v-if="!hidden" class="text-danger"> *</span>
+                    <span  v-Tooltip.top="tooltip" v-if="tooltip">
+                        <i class="pi pi-question-circle"></i>
+                    </span>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-2  d-md-flex justify-content-end" v-if="hidden">
+                    <Button icon="pi pi-eye" severity="help" rounded outlined  aria-label="Favorite" @click="handleNaoAplica(componentKey)" />
+                </div>
+            </div>
         </template>
-        <template #subtitle>{{ hint }}</template>
+        <template #subtitle>
+            {{ hint }}
+        </template>
         <template #content>
             <div class="col-lg-12 col-md-12 col-sm-12 col-12">
-                <InputText v-if="component === 'TEXT_INPUT' || component === 'NUMBER_INPUT' || component === 'NUMBER_UNIT_INPUT' " v-model="value"  size="large" class="input-text-main-features" :id="name + componentKey"/>  
-                <Dropdown v-if="component === 'COMBO' || component === 'COLOR_INPUT' || component === 'TEXT_OUTPUT'" v-model="selected" :options="options" optionLabel="name" placeholder="" class="w-full md:w-14rem"  :id="name + componentKey" />            
-                <InputNumber  inputId="locale-user" prefix="R$ " :minFractionDigits="2" v-if="component === 'CURRENCY_INPUT' " v-model="currency"  size="large" class="input-text-main-features"  :id="name + componentKey"/>  
+                <InputText v-if="component === 'TEXT_INPUT' || component === 'NUMBER_INPUT' || component === 'NUMBER_UNIT_INPUT' " v-model="value"  size="large" class="input-text-main-features" :class="{ 'p-invalid': invalid }" :id="name + componentKey"/>  
+                <Dropdown v-if="component === 'COMBO' || component === 'COLOR_INPUT' || component === 'TEXT_OUTPUT'" v-model="value" :options="options" optionLabel="name" placeholder="" class="w-full md:w-14rem" :class="{ 'p-invalid': invalid }" :id="name + componentKey" />            
+                <InputNumber  inputId="locale-user" prefix="R$ " :minFractionDigits="2" v-if="component === 'CURRENCY_INPUT'" v-model="currency"  size="large" class="input-text-main-features" :class="{ 'p-invalid': invalid }" :id="name + componentKey"/>  
             </div>
             <div class="row" v-if="component === 'BOOLEAN_INPUT'">
                 <div class="col-lg-3 col-md-3 col-sm-3 col-3" ></div>
@@ -35,7 +45,7 @@
                 <div class="col-lg-3 col-md-3 col-sm-3 col-3 d-grid gap-3 d-md-flex justify-content-end"></div> 
                 <div class="col-lg-3 col-md-3 col-sm-3 col-3 d-grid gap-3 d-md-flex justify-content-end"></div>
                 <div class="col-lg-3 col-md-3 col-sm-3 col-3 d-grid gap-3 d-md-flex justify-content-end">
-                    <Button label="Confirmar" @click="handleConfirm(name, this.value, this.selected, this.currency, attribute_id, componentKey)" size="large" class="float-end" />
+                    <Button label="Confirmar" outlined :disabled="btnDisabled" @click="handleConfirm(name, !this.value ? this.currency : this.value, attribute_id, componentKey, !this.hidden)" size="large" class="float-end" />
                 </div>
             </div>
         </template>
@@ -76,22 +86,36 @@ export default {
         attribute_id: {
             type:Number,
             required: true
+        },
+        hidden: {
+            type: Boolean,
+            required: true
         }
     },
     data() {
         return {
             value: '',
             selected: '',
-            currency: 0.00
+            currency: 0.00,
+            btnDisabled: false,
+            invalid: false            
         };
     },
     methods: {
-        async handleConfirm(nameProp, valueProp, selectedProp, currency, attibuteId, position){
-            valueProp = currency ?? valueProp;
-            this.$emit('handleConfirm', {id: attibuteId, name: nameProp.toLowerCase() , value: (valueProp ? valueProp : selectedProp), position: position});
+        async handleConfirm(nameProp, valueProp, attibuteId, position, required){
+            this.invalid = false;
+            if(required && !valueProp){
+                this.invalid = true;
+                return false; 
+            }
+            this.$emit('handleConfirm', {id: attibuteId, name: nameProp.toLowerCase() , value: valueProp, position: position, required: required});
         },
         async handleGetSelected( nameProp, valueProp ){
             this.$emit('handleGetSelected', { name: nameProp, value: valueProp });
+        },
+        async handleNaoAplica( position ){
+            this.$emit('handleNaoAplica', position);
+            console.log(position)
         }
     },   
     emits: ['handleConfirm','handleGetSelected']
