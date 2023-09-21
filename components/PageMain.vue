1dc2cfe9a78c2e686a67da69c8699fd11c1ad1b2
jeferson2020 <jeferson@lopessoftware.com.br>
2023-09-21 00:12:29 -0300

    ajutes

--- components/PageMain.vue
+++ components/PageMain.vue
@@ -1,10 +1,14 @@
 <template> 
-    <div>
-        <div class="titulo">Baseada na sua última visita <NuxtLink class="min-titulo">Ver Histórico</NuxtLink> </div>        
+    <CarouselCustom></CarouselCustom>
+    <br><br>
+    <div class="row g-8 justify-content-lg-center">
+        <div class="col-lg-10">
+            <div class="titulo">Baseada na sua última visita <NuxtLink class="min-titulo">Ver Histórico</NuxtLink> </div>        
+        </div>
     </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <Carousel :value="viewproducts" :numVisible="4" :numScroll="4">
+    <div class="row justify-content-lg-center">
+        <div class="col-lg-10">
+            <Carousel :value="viewproducts" :numVisible="5" :numScroll="5">
                 <template #item="slotProps">
                     <NuxtLink :to="`products?token=${slotProps.data.token}`">
                         <div class="border-1 surface-border border-round m-1 bg-custom">
@@ -28,25 +32,33 @@
             </Carousel>
         </div>
     </div>
-    <h2 class="">Ofertas do dia <a class="min-titulo">Ver Histórico</a> </h2>
-    <div class="row">
-        <div class="col-lg-12">
-            <Carousel :value="products" :numVisible="4" :numScroll="4" >
+    <div class="row g-8 justify-content-lg-center">
+        <div class="col-lg-10">
+            <div class="titulo">Ofertas do dias <NuxtLink class="min-titulo">Ver Histórico</NuxtLink> </div>        
+        </div>
+    </div>
+    <div class="row g-8 justify-content-lg-center">
+        <div class="col-lg-10">
+            <Carousel :value="viewproducts" :numVisible="4" :numScroll="4">
                 <template #item="slotProps">
-                    <div class="border-1 surface-border border-round m-1 bg-custom">
-                        <div class="mb-3">
-                            <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-12"/>
+                    <NuxtLink :to="`products?token=${slotProps.data.token}`">
+                        <div class="border-1 surface-border border-round m-1 bg-custom">
+                            <div class="mb-3">
+                                <img :src="slotProps.data.thumbnail" :alt="slotProps.data.name" class="w-12"/>
+                            </div>
+                            <div>
+                                <span class="price-size textteste">R$ {{ slotProps.data.original_price }} </span>
+                            </div>
+                            <div class="ui-recommendations-card__price-and-discount">
+                                <span class="price-size textteste">R$ {{ slotProps.data.price }} </span> 
+                                <span class="cents andes-money-amount__cents andes-money-amount__cents--superscript-24"> {{ slotProps.data.cent }}</span>                           
+                                <span class="installments">{{ slotProps.data.discount }} </span>
+                            </div>
+                            <div class="installments">{{ slotProps.data.parcela }}</div>
+                            <div class="installments">{{ slotProps.data.frete }}</div>
+                            <div class="textteste">{{ slotProps.data.title }}</div>
                         </div>
-                        <div class="ui-recommendations-card__price-and-discount">                           
-                            <span class="price-size textteste">R$ {{ slotProps.data.price }} </span> 
-                            <span class="cents andes-money-amount__cents andes-money-amount__cents--superscript-24"> {{ slotProps.data.cent }}</span>                           
-                            <span class="installments">{{ slotProps.data.discount }} </span>
-                        </div>
-                        <div class="installments">{{ slotProps.data.parcela }}</div>
-                        <div class="installments">{{ slotProps.data.frete }}</div>
-                        <span class="pix">5% OFF com Mercado Crédito</span>
-                        <div class="textteste">{{ slotProps.data.name }}</div>                        
-                    </div>
+                    </NuxtLink>
                 </template>
             </Carousel>
         </div>
