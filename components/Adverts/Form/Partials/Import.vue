4fed0ee58678cc927e1f54cf435abff4ee477f89
jeferson2020 <jeferson@lopessoftware.com.br>
2023-11-09 01:01:41 -0300

    Import

--- components/Adverts/Form/Partials/Import.vue
+++ components/Adverts/Form/Partials/Import.vue
@@ -5,18 +5,12 @@
                 <form class="panel">
                     <div class="row g-4">
                         <div class="col-lg-4 col-md-4 col-sm-4 col-4">
-                            <h4 >Total de itens: {{ formData.total ?? 0 }} </h4> 
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-4">
-                            <h4 >Total de páginas: {{  formData.primary_results ?? 0 }} </h4> 
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-4">
-                            <h4 >Total de items por página: {{ formData.limit ?? 0 }} </h4> 
+                            <h4 >Total de itens: {{ this.totalItems }}</h4> 
                         </div>
                     </div>
                     <div class="row g-4">
                         <div class="col-lg-4 col-md-4 col-sm-4 col-4">
-                            <h4> Concluidos: {{ this.concluidos }}</h4>                           
+                            <h4> Concluidos: {{ this.concluidos }}</h4>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-4 col-4">
                             <h4> Pendentes: {{ this.pendentes }}</h4>
@@ -61,7 +55,8 @@
                 concluidos: 0,
                 pendentes: 0,
                 processando: 0,
-                porcentagem: 0
+                porcentagem: 0,
+                totalItems: 0
             }
         },
         methods:{            
@@ -70,7 +65,7 @@
                 this.isProgressBar = true;
                 const productservice = new ProductService();
                 const form = new FormData();
-                this.entityId =  localStorage.getItem('entityId');
+                this.entityId = localStorage.getItem('entityId');
 
                 if (!this.entityId){
                     return false;
@@ -82,8 +77,10 @@
                 let status = responseData.value ? responseData._rawValue.status : null;
                 status = status ?? (responseError.value ? responseError.value.statusCode : null);
 
-                if (status === 201){        
-                    this.formData = responseData._rawValue.data; 
+                if (status === 201){
+                    this.formData = responseData._rawValue.data;
+                    this.concluidos = this.formData[1].imported.imported; 
+                    this.totalItems = this.formData[1].import.toImport; 
                     this.isImport = true
                     this.isProgressBar = false;
                     this.message = 'Importação concluida!';              
@@ -93,7 +90,7 @@
                     this.isProgressBar = false;
                 }
             },
-            async handleImport(offset){
+            async handleStart(){
                 const productservice = new ProductService();
                 const form = new FormData();
                 this.entityId =  localStorage.getItem('entityId');
@@ -103,7 +100,6 @@
                 }
 
                 form.append('entity_id', this.entityId);
-                form.append('offset', offset);
                 
                 const { data: responseData, error: responseError } = await productservice.importItems(form);
                 let status = responseData.value ? responseData._rawValue.status : null;
@@ -118,22 +114,7 @@
                 if (status > 200){  
                     this.isProgressBar = false;
                 }
-            },
-            async handleStart(){
-                const page = (this.formData.total+1);
-                let contador = 0;
-                for(let i = 0; i < page; i++ ){
-                    this.processando = 1;
-                    const result = await this.handleImport(i);
-                    if(result){                        
-                        contador += result;
-                        this.concluidos = contador;
-                    } 
-                    console.log(this.concluidos)
-                    this.pendentes = (page - this.concluidos);
-                    this.porcentagem = Math.round((this.concluidos / page) * 100);
-                }
-            },
+            }
         },
         mounted(){                
         }
